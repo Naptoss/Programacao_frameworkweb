fc6bd4070fc61bfac411c08fcd34032e70062be1
Naptoss <tomgabinio@gmail.com>
2023-09-03 13:52:09 -0300

    Done

--- scripts.js
+++ scripts.js
@@ -1,41 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     var cabecalhosBtn = document.getElementById('cabecalhosBtn');
-    var  cabecalho = document.getElementById('cabecalho');
-    for(var i = 1; i <= 6; i++) {
-        cabecalhosBtn.addEventListener('click', function() {
-            event.preventDefault();
+    var cabecalhoResult = document.getElementById('cabecalhoResult');
+
+    cabecalhosBtn.addEventListener('click', function() {
+        event.preventDefault();
+        cabecalhoResult.innerHTML = ''; // Limpa o conteúdo existente
+        for (var i = 1; i<= 6; i++) {
             var cabecalho = document.createElement('h' + i);
             cabecalho.innerHTML = 'Cabeçalho ' + i;
-            document.body.appendChild(cabecalho);
-        });
-    }
-});
+            cabecalhoResult.appendChild(cabecalho);
+        }
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
     var VerificarBtn = document.getElementById('VerificarBtn');
     var resultado = document.getElementById('resultado');
-    VerificarBtn.addEventListener('click', function() {
+
+    VerificarBtn.addEventListener('click', function(event) {
         event.preventDefault();
         var idade = document.getElementById('idade').value;
-        if (idade >=18){
-            resultado.innerHTML = 'Você é maior de idade';
-        } else if (idade  !== null) {
+        
+        if (idade === '') {
             resultado.innerHTML = 'Adicione sua idade';
-        }else {
+        } else if (idade >= 18) {
+            resultado.innerHTML = 'Você é maior de idade';
+        } else {
             resultado.innerHTML = 'Você é menor de idade';
         }
-
     });
 });
